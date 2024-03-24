7d7a79c49af53a7b94aaa054eb48d26353df88d4
RGuide <bunditsrisakul@gmail.com>
2024-03-24 21:51:22 +0700

    Add Login Page not linked

---
+++ src/app/Login/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import LoginPanel from '@/components/AuthPage/LoginPanel'
+
+const SignInPage = () => {
+    return (
+        <div className="w-[60%] bg-white rounded-md drop-shadow-md flex flex-col justify-center mx-auto my-10 px-10 py-8">
+            <div className="text-[#128281] text-sm mb-7">
+                <div className="text-xl font-bold">Sign In</div>
+                ยินดีต้อนรับสู่ หมอฟันการแพทย์ สุดทันสมัย
+            </div>
+            <LoginPanel/>
+        </div>
+    );
+};
+
+export default SignInPage;
