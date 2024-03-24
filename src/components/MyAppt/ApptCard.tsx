2e33a216581416877ab1980d3269df1f82fcd4da
Alknot1122 <117248001+Alknot1122@users.noreply.github.com>
2024-03-24 21:19:17 +0700

    + Appt fetch from db

--- src/components/MyAppt/ApptCard.tsx
+++ src/components/MyAppt/ApptCard.tsx
@@ -1,18 +1,26 @@
+import { getDentist } from "@/lib/getDentists";
 export interface ApptInfoProps {
-    pastDate: string;
-    work: string;
-    doctor : string;
-    createAt: Date;
+    appointmentDate: string;
+    work: string| null;
+    dentist : string;
+    createdAt: Date;
 }
 
-export default function ApptCard(props: ApptInfoProps) {
+export default async function ApptCard(props: ApptInfoProps) {
+
+    const dentistData = await getDentist();
+    const dentistName = dentistData.data.find((dentist: { id: string }) => dentist.id === props.dentist)?.name;
+    const dentistWork = dentistData.data.find((dentist: { id: string }) => dentist.id === props.dentist)?.areaOfExpertise;
+    const date = new Date(props.appointmentDate).toLocaleString()
+    const createdDate = new Date(props.createdAt).toLocaleString()
+
     return (
         <div className="w-[100%] h-[170px] bg-[#FFB6B9] text-[#FFFFFF] rounded-md drop-shadow-md my-2 px-4 py-3 ">
-            <div className="text-[20pt] font-bold">นัดที่กำลังจะมาถึง {props.pastDate}</div>
+            <div className="text-[20pt] font-bold">นัดที่กำลังจะมาถึง {date}</div>
             <div className=" pl-5 text-[16pt]"> 
-                <span className="font-bold">หัวข้อ : </span>{props.work} <br></br>
-                <span className="font-bold">หมอ : </span>{props.doctor} <br></br>
-                <span className="font-bold">สร้างเมื่อ : </span>{props.createAt.toLocaleDateString()} <br></br>
+                <span className="font-bold">หัวข้อ : </span>{dentistWork} <br></br>
+                <span className="font-bold">หมอ : </span>{dentistName} <br></br>
+                <span className="font-bold">สร้างเมื่อ : </span>{createdDate} <br></br>
             </div>
         </div>
     );
--- src/components/MyAppt/ApptPanel.tsx
+++ src/components/MyAppt/ApptPanel.tsx
@@ -1,18 +1,17 @@
+import { getAppointments } from "@/lib/getAppointments";
+import { getDentist } from "@/lib/getDentists";
 import ApptCard , { ApptInfoProps } from "./ApptCard";
 
-const ApptList: ApptInfoProps[] = [
-    {
-        pastDate: "วันที่ 1"
-        , work: "เจาะปาก"
-        , doctor: "โชค โบกไปมา"
-        , createAt: new Date("2022-01-01")
-    }
-];
+export default async function ApptPanel() {
 
-export default function ApptPanel(){
+  let appointment = await getAppointments();
+  const appointmentData: ApptInfoProps[] = appointment.data;
+  console.log(appointmentData);
+  const dentistData = getDentist();
+  
     return (
         <div className="flex flex-col justify-center w-[80%] mx-auto py-2">
-          {ApptList.map((card, index) => {
+          {appointmentData.map((card, index) => {
             return <ApptCard key={index} {...card} />;
           })}
         </div>
