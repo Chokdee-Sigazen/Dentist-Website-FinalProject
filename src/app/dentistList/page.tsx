f1cab6ce12f91e3e4f78f31a640b105c59131ac8
Sigazen <choikdee50@gmail.com>
2024-03-24 19:48:30 +0700

    add fetch

--- src/app/dentistList/page.tsx
+++ src/app/dentistList/page.tsx
@@ -1,15 +1,15 @@
 import DentistPanel from "@/components/DentistList/DentistPanel";
-// import { getDentist } from "@/lib/getDentists";
+import { getDentist } from "@/lib/getDentists";
 
 export default async function DentistList() {
-  // const dentistList = await getDentist();
+  const dentistList = await getDentist();
 
   return (
     <main className="w-[100%] flex flex-col items-center bg-[#FAE3D9] rounded-md mx-4 my-4 px-4 drop-shadow-sm">
       <div className="text-2xl font-bold text-[#895355] font-bolder py-5">
         Dentist List
       </div>
-      {/* <DentistPanel DentistList={dentistList.data}></DentistPanel> */}
+      <DentistPanel DentistList={dentistList.data}></DentistPanel>
     </main>
   );
 }
