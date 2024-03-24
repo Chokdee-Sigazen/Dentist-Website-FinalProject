8a91993cc8ba8ad1d698ccf464f0e62324dc0c85
Sigazen <choikdee50@gmail.com>
2024-03-24 19:44:54 +0700

    add api

--- src/lib/getDentists.ts
+++ src/lib/getDentists.ts
@@ -1,12 +1,12 @@
-export async function getDentist() {
-    await new Promise((resolve) => setTimeout(resolve, 1000));
+// export async function getDentist() {
+//     await new Promise((resolve) => setTimeout(resolve, 1000));
 
-    const response = await fetch("https://dentist-website-final-project.vercel.app/api/dentists");
+//     const response = await fetch("https://dentist-website-final-project.vercel.app/api/dentists");
    
-    if (!response.ok) {
-        throw new Error("Error failed to fetch");
-    }
-    const data = await response.json()
-    console.log(data)
-    return  data;
-}
+//     if (!response.ok) {
+//         throw new Error("Error failed to fetch");
+//     }
+//     const data = await response.json()
+//     console.log(data)
+//     return  data;
+// }
--- src/lib/getUsers.ts
+++ src/lib/getUsers.ts
@@ -1,13 +1,13 @@
 
-export  async function getUsers() {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
+// export  async function getUsers() {
+//   await new Promise((resolve) => setTimeout(resolve, 1000));
 
-  const response = await fetch(
-    "https://dentist-website-final-project.vercel.app/api/users"
-  );
-  if (!response.ok) {
-    throw new Error("Error failed to fetch");
-  }
+//   const response = await fetch(
+//     "https://dentist-website-final-project.vercel.app/api/users"
+//   );
+//   if (!response.ok) {
+//     throw new Error("Error failed to fetch");
+//   }
 
-  return await response.json();
-}
+//   return await response.json();
+// }
