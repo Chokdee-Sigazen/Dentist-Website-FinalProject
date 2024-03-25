3ef811c61ab8003af711cd02b31bee0d623ad4c9
RGuide <bunditsrisakul@gmail.com>
2024-03-25 15:50:10 +0700

    Add logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,9 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <section className="fixed z-50 top-0 left-0 bg-[#FFFFFF] justify-between flex h-14 w-full py-2 px-6">
-      <div className="h-10 w-24 p-2 bg-[#72A3A2]"></div>
+      <Link href="/">
+          <img src="/images/logo.png" className="h-[100%] rounded-md drop-shadow-md"></img>
+      </Link>
       <section className="flex space-x-2 text-[8pt] md:text-[10pt] lg:text-[12pt] items-center">
         <Link href="/makeAppointment" className="p-2 text-[#835A5C] font-bold hover:underline">
           Create Appointment
