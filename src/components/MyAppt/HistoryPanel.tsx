04d73ce795f25acd19f5864ae11e32ededf93beb
Alknot1122 <117248001+Alknot1122@users.noreply.github.com>
2024-03-25 17:49:31 +0700

    + Myappointment history

--- src/components/MyAppt/HistoryPanel.tsx
+++ src/components/MyAppt/HistoryPanel.tsx
@@ -1,25 +1,18 @@
-import ApptCard , { ApptInfoProps } from "./PastApptCard";
+import { getAppointments } from "@/lib/getAppointments";
+import PastApptCard , { ApptInfoProps } from "./PastApptCard";
 
-const HistoryList: ApptInfoProps[] = [
-    {
-        pastDate: "วันที่ 1"
-        , work: "เจาะปาก"
-        , doctor: "โชค โบกไปมา"
-        , createAt: new Date("2022-01-01")
-    },
-    {
-        pastDate: "วันที่ 2"
-        , work: "ถอนฟัน"
-        , doctor: "อมร นอนไม่พอ"
-        , createAt: new Date("2022-01-02")
-    }
-];
+export default async function PastApptPanel() {
 
-export default function HistoryPanel(){
+  let appointments = await getAppointments();
+  const appointmentData: ApptInfoProps[] = appointments.data.filter(
+    (appointment: ApptInfoProps) => appointment.finish
+  );
+
+  
     return (
         <div className="flex flex-col justify-center w-[80%] mx-auto py-2">
-          {HistoryList.map((card, index) => {
-            return <ApptCard key={index} {...card} />;
+          {appointmentData.map((card, index) => {
+            return <PastApptCard key={index} {...card} />;
           })}
         </div>
       );
--- src/components/MyAppt/PastApptCard.tsx
+++ src/components/MyAppt/PastApptCard.tsx
@@ -1,19 +1,31 @@
+import { getDentist } from "@/lib/getDentists";
 export interface ApptInfoProps {
-    pastDate: string;
-    work: string;
-    doctor : string;
-    createAt: Date;
+    appointmentDate: string;
+    work: string| null;
+    dentist : string;
+    createdAt: Date;
+    finish: boolean;
 }
 
-export default function ApptCard(props: ApptInfoProps) {
+export default async function PastApptCard(props: ApptInfoProps) {
+
+    const dentistData = await getDentist();
+    const dentistName = dentistData.data.find((dentist: { id: string }) => dentist.id === props.dentist)?.name;
+    const dentistWork = dentistData.data.find((dentist: { id: string }) => dentist.id === props.dentist)?.areaOfExpertise;
+    const date = new Date(props.appointmentDate).toLocaleString()
+    const createdDate = new Date(props.createdAt).toLocaleString()
+
     return (
-        <div className="w-[100%] bg-[#61C0BF] text-[#FFFFFF] rounded-md drop-shadow-md my-2 px-4 py-3 ">
-            <div className="text-[15pt] font-bold">นัดที่ผ่านมา {props.pastDate}</div>
-            <div className="pl-5 text-[12pt]"> 
-                <span className="font-bold">หัวข้อ : </span>{props.work} <br></br>
-                <span className="font-bold">หมอ : </span>{props.doctor} <br></br>
-                <span className="font-bold">สร้างเมื่อ : </span>{props.createAt.toLocaleDateString()} <br></br>
+        <div className="w-[100%] h-[170px] bg-[#61C0BF] text-[#FFFFFF] rounded-md drop-shadow-md my-2 px-4 py-3 ">
+            <div className="text-[20pt] font-bold">นัดที่กำลังจะมาถึง {date}</div>
+            <div className=" pl-5 text-[16pt]"> 
+                <span className="font-bold">หัวข้อ : </span>{dentistWork} <br></br>
+                <span className="font-bold">หมอ : </span>{dentistName} <br></br>
+                <span className="font-bold">สร้างเมื่อ : </span>{createdDate} <br></br>
+                <span className="font-bold">เสร็จ : </span>{props.finish ? "เสร็จแล้ว" : "ยังไม่เสร็จ"} <br></br>
             </div>
         </div>
     );
 }
+
+
