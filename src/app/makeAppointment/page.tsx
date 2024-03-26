b9e7d19a158166c0c7508d4812d71b9957a5c69a
Sigazen <choikdee50@gmail.com>
2024-03-26 09:57:06 +0700

    fix make appointment

--- src/app/makeAppointment/page.tsx
+++ src/app/makeAppointment/page.tsx
@@ -2,7 +2,7 @@
 import FormPanel from "@/components/MakeAppt/FormPanel";
 export default function makeAppt() {
   return (
-    <main className="w-[97%] flex flex-col items-center bg-[#FAE3D9] rounded-md mx-auto my-4 px-4 drop-shadow-sm">
+    <main className="w-[97%] min-h-[87vh] flex flex-col items-center bg-[#FAE3D9] rounded-md mx-auto my-4 px-4 drop-shadow-sm">
       <div className="text-2xl font-bold text-[#895355] font-bolder py-5">
         Make Appointment
       </div>
