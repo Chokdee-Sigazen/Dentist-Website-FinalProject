de6d0ff9dc0f6d84ebe061bf48e81b90a1aebaad
Sigazen <choikdee50@gmail.com>
2024-03-27 10:31:22 +0700

    add

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 export {default} from "next-auth/middleware"
 
 export const config = {
-    matcher : ["/myAppointment","/makeAppointment"]
+    matcher : ["/myAppointment","/makeAppointment","/profile"]
 }
