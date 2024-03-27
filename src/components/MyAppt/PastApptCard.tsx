489bc2001c55f1e7f5b0faa11a72d75e1c27e01f
Sigazen <choikdee50@gmail.com>
2024-03-27 10:32:35 +0700

    final

--- src/components/MyAppt/PastApptCard.tsx
+++ src/components/MyAppt/PastApptCard.tsx
@@ -1,31 +1,37 @@
 import { getDentist } from "@/lib/getDentists";
 export interface ApptInfoProps {
-    user: string;
-    appointmentDate: string;
-    work: string| null;
-    dentist : string;
-    createdAt: Date;
-    finish: boolean;
+  user: string;
+  appointmentDate: string;
+  work: string | null;
+  dentist: string;
+  createdAt: Date;
+  finish: boolean;
 }
 
 export default async function PastApptCard(props: ApptInfoProps) {
+  const dentistData = await getDentist();
+  const dentistName = dentistData.data.find(
+    (dentist: { id: string }) => dentist.id === props.dentist
+  )?.name;
+  const dentistWork = dentistData.data.find(
+    (dentist: { id: string }) => dentist.id === props.dentist
+  )?.areaOfExpertise;
+  const date = new Date(props.appointmentDate).toLocaleString();
+  const createdDate = new Date(props.createdAt).toLocaleString();
 
-    const dentistData = await getDentist();
-    const dentistName = dentistData.data.find((dentist: { id: string }) => dentist.id === props.dentist)?.name;
-    const dentistWork = dentistData.data.find((dentist: { id: string }) => dentist.id === props.dentist)?.areaOfExpertise;
-    const date = new Date(props.appointmentDate).toLocaleString()
-    const createdDate = new Date(props.createdAt).toLocaleString()
-
-    return (
-        <div className="w-[100%] h-auto bg-[#61C0BF] text-[#FFFFFF] rounded-md drop-shadow-md my-2 px-4 py-3 ">
-            <div className="text-[16pt] sm:text-[18pt] md:text-[20pt] font-bold">นัดที่ผ่านมา : {date}</div>
-            <div className=" pl-5 text-[13pt] sm:text-[15pt] md:text-[18pt]"> 
-                <span className="font-bold">หัวข้อ : </span>{dentistWork} <br></br>
-                <span className="font-bold">หมอ : </span>{dentistName} <br></br>
-                <span className="font-bold">สร้างเมื่อ : </span>{createdDate} <br></br>
-            </div>
-        </div>
-    );
+  return (
+    <div className="w-[100%] h-auto bg-[#61C0BF] text-[#FFFFFF] rounded-md drop-shadow-md my-2 px-4 py-3 ">
+      <div className="text-[16pt] sm:text-[18pt] md:text-[20pt] font-bold">
+        นัดที่ผ่านมา : {date}
+      </div>
+      <div className=" pl-5 text-[13pt] sm:text-[15pt] md:text-[18pt]">
+        <span className="font-bold">หัวข้อ : </span>
+        {dentistWork} <br></br>
+        <span className="font-bold">หมอ : </span>
+        {dentistName} <br></br>
+        <span className="font-bold">สร้างเมื่อ : </span>
+        {createdDate} <br></br>
+      </div>
+    </div>
+  );
 }
-
-
